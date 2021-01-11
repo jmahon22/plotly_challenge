d8648c743be4bb6d5be902a0e882202de89df817
Jen Mahon <Jen.Mahon@villageinn.com>
2021-01-11 11:30:43 -0700

    more work on code

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 // use D3 library to read in samples.json
 function demographicInfo(sample){
     
-// fetch the metadata using d3.json
+// demographic info display
     d3.json("data/samples.json").then((data) => {
         console.log(data);
                
@@ -134,7 +134,63 @@ function DropDownMenu() {
 //     };
   
 //     Plotly.newPlot("bubble", data, layout);
+
+// //gauge chart
+
+//     var metaData = data.metadata;
+//     console.log(metaData);
+
+//   // filter metadataID for one or more results
+//     var metaDataID = metaData.filter(item => item.id == sample);
+//         console.log('HERE');
+//         console.log(metaDataID[0].wfreq); 
+
+//     var gaugechart = [
+//       {
+//         domain: { x: [0, 1], y: [0, 1] },
+//         value: metaDataID[0].wfreq,
+//         title: { text: "<b>Belly Button Washing Frequency</b><br>Scrubs Per Week" },
+//         titlefont: {family: 'Arial, Helvetica, sans-serif'},
+        
+        
+//         // text: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
+//         // textinfo: 'text',
+//         // textposition:'inside',
+//         // textfont:{
+//         //  size : 16,
+//         // },
+        
+//         type: "indicator",
+//         mode: "gauge+number",
+//         gauge: {
+//           axis: { range: [null, 10] },
+//           bar: {color: "red"},
+//           steps: [
+//             { range: [0, 1], color: 'rgb(258, 250, 245)'},
+//             { range: [1, 2], color: 'rgb(238, 235, 222)' },
+//             { range: [2, 3], color: 'rgb(225, 225, 180)' },
+//             { range: [3, 4], color: 'rgb(229, 231, 163)' },
+//             { range: [4, 5], color: 'rgb(213, 228, 157)' },
+//             { range: [5, 6], color: 'rgb(183, 204, 146)' },
+//             { range: [6, 7], color: 'rgb(150, 191, 136)' },
+//             { range: [7, 8], color: 'rgb(140, 185, 126)'  },
+//             { range: [8, 9], color: 'rgb(128, 178, 112)' },
+//             { range: [9, 10], color: 'rgb(110, 167, 108)' }
+//           ],
+      
+//       },
+//     },
+//       ];
   
+//     var data = gaugechart;
+
+//     var layout = { width: 600, 
+//                 height: 500, 
+//                 margin: { t: 0, b: 0 } 
+//                 };
+
+//     Plotly.newPlot('gauge', data, layout);
+
 //     });
   
 // };
